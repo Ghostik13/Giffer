612c518637aef361caa92e20af6a682ad6f862af
Ghostik13 <roxan95@mail.ru>
2021-04-03 00:34:04 +0300

    demo version

--- app/src/main/java/com/example/giffer/MainActivity.kt
+++ app/src/main/java/com/example/giffer/MainActivity.kt
@@ -2,24 +2,19 @@ package com.example.giffer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.giffer.adapter.GifAdapter
 import com.example.giffer.repository.Repository
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.gif_item.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private val adapter by lazy {
-        GifAdapter()
-    }
+    private val adapter by lazy { GifAdapter() }
     private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +24,32 @@ class MainActivity : AppCompatActivity() {
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+        initTrendGifs()
+        initSearchGifs()
+    }
+
+    private fun initRecyclerView() {
+        recyclerView = recycler_view
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+    }
+
+    private fun initSearchGifs() {
+        search_btn.setOnClickListener {
+            val category = edit_text.text.toString()
+            viewModel.getGifs(category)
+            viewModel.gifResponse.observe(this, Observer { response ->
+                if (response.isSuccessful) {
+                    response.body()?.data?.let { adapter.setData(it) }
+                } else {
+                    Toast.makeText(this, response.code(), Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+    }
+
+    private fun initTrendGifs() {
         viewModel.getTrendGifs()
         viewModel.gifTrendResponse.observe(this, Observer { response ->
             if (response.isSuccessful) {
@@ -37,11 +58,5 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, response.code(), Toast.LENGTH_SHORT).show()
             }
         })
-    }
-
-    private fun initRecyclerView() {
-        recyclerView = recycler_view
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
     }
 }
--- app/src/main/java/com/example/giffer/adapter/GifAdapter.kt
+++ app/src/main/java/com/example/giffer/adapter/GifAdapter.kt
@@ -4,18 +4,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
-import com.example.giffer.MainActivity
 import com.example.giffer.model.Data
-import com.example.giffer.model.MainGif
 import kotlinx.android.synthetic.main.gif_item.view.*
 
-class GifAdapter: RecyclerView.Adapter<GifAdapter.GifViewHolder>(){
+class GifAdapter : RecyclerView.Adapter<GifAdapter.GifViewHolder>() {
 
     private var gifList = emptyList<Data>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GifViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(com.example.giffer.R.layout.gif_item, parent, false)
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(
+                com.example.giffer.R.layout.gif_item,
+                parent,
+                false
+            )
         return GifViewHolder(view)
     }
 
@@ -24,18 +29,29 @@ class GifAdapter: RecyclerView.Adapter<GifAdapter.GifViewHolder>(){
     }
 
     override fun onBindViewHolder(holder: GifViewHolder, position: Int) {
+        setGifToHolder(holder, position)
+    }
+
+    private fun setGifToHolder(
+        holder: GifViewHolder,
+        position: Int
+    ) {
+        val circularProgressDrawable = CircularProgressDrawable(holder.itemView.gif_element.context)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
         Glide
             .with(holder.itemView.gif_element.context)
             .load(gifList[position].images.original.url)
+            .placeholder(circularProgressDrawable)
             .into(holder.itemView.gif_element)
-        holder.itemView.progress_bar.visibility = View.INVISIBLE
     }
 
-    fun setData(gifList: List<Data>){
+    fun setData(gifList: List<Data>) {
         this.gifList = gifList
         notifyDataSetChanged()
     }
 
-    class GifViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    class GifViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
 }
